9621fa01f6044f743becc481c8e882310db5446e
Francis420 <francisjonel.balagusa@evsu.edu.ph>
2024-12-08 13:15:38 +0800

    Fixed css not loading for daphne

--- notifications/consumers.py
+++ notifications/consumers.py
@@ -3,15 +3,18 @@ from channels.generic.websocket import AsyncWebsocketConsumer
 
 class NotificationConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        self.user = self.scope["user"]
-        self.group_name = f"notifications_{self.user.id}"
+        if self.scope["user"].is_anonymous:
+            await self.close()
+        else:
+            self.user = self.scope["user"]
+            self.group_name = f"notifications_{self.user.id}"
 
-        await self.channel_layer.group_add(
-            self.group_name,
-            self.channel_name
-        )
+            await self.channel_layer.group_add(
+                self.group_name,
+                self.channel_name
+            )
 
-        await self.accept()
+            await self.accept()
 
     async def disconnect(self, close_code):
         await self.channel_layer.group_discard(
